591eeaf811b36bd4a8179c09472a41b0306880a3
HieuLe <152293787+pak-youu@users.noreply.github.com>
2025-09-02 16:40:27 +0700

    update cors

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", "https://evalcode.vercel.app",
+    origin: ["http://localhost:5173", "https://evalcode.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
